307ee6e81a5af491bae00deea58f136f80cfd975
Mahdi Golestan <Mahdi Golestan>
2024-07-12 14:04:46 +0330

    use Minimal Api

--- Adapter Design Pattern Example/Program.cs
+++ Adapter Design Pattern Example/Program.cs
@@ -1,5 +1,3 @@
-
-using Adapter_Design_Pattern_Example.Controllers;
 using Adapter_Design_Pattern_Example.Services;
 using Adapter_Design_Pattern_Example.Services.PaymentAdapter;
 
@@ -12,12 +10,11 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
+        // Register payment services
         builder.Services.AddScoped<IZarinPalPayment, ZarinPalPayment>();
         builder.Services.AddScoped<ISamanPayment, SamanPayment>();
 
@@ -37,9 +34,30 @@ public class Program
 
         app.UseAuthorization();
 
-
+        // Map Controllers
         app.MapControllers();
+
+        // Map minimal API endpoint
+        app.MapPost("/api/payment", (PaymentModel model, IServiceProvider serviceProvider) =>
+        {
+            var orderPaymentService = serviceProvider.GetRequiredKeyedService<IPaymentOrderAdapter>(model.PaymentGetWay.ToString());
+            var result = orderPaymentService.Pay(model.Amount, model.OrderId);
+            return Results.Content(result);
+        });
 
         app.Run();
     }
+}
+
+public class PaymentModel
+{
+    public int OrderId { get; set; }
+    public decimal Amount { get; set; }
+    public PaymentGetWay PaymentGetWay { get; set; }
+}
+
+public enum PaymentGetWay
+{
+    zarinpay,
+    samanpay
 }
